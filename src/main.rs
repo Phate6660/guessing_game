7449152cd47e11fc2fbe83f2d757cff959f39326
Ash <ValleyKnight@protonmail.com>
2020-03-04 01:54:40 -0500

    format with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use std::io;
+use rand::Rng;
 use std::cmp::Ordering;
-use rand::Rng; // from the rand crate specified in Cargo.toml
+use std::io; // from the rand crate specified in Cargo.toml
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101); // generate a random number between 1 and 101
-   
+
     // loop the input and result
     loop {
         println!("Please input your guess.");
-    
+
         let mut guess = String::new(); // create mutable variable with the contents of a new string
-        io::stdin().read_line(&mut guess) // variable guess is filled with contents from stdin (e.g. user input)
+        io::stdin()
+            .read_line(&mut guess) // variable guess is filled with contents from stdin (e.g. user input)
             .expect("Failed to read line"); // catch the error if one appears
 
         // ensure guess is an unsigned 32-bit integer
@@ -20,14 +21,14 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
+
         println!("You guessed: {}", guess);
 
         // compare the guess to the secret number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => { 
+            Ordering::Equal => {
                 println!("You win!");
                 break; // break out of the loop if the right number is guessed
             }
